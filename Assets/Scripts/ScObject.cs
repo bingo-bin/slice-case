ef930467439bec06b700f7c08f2ff621b6136ff4
latufla.work <latufla.work@gmail.com>
2020-05-28 13:44:48 +0700

    initial commit

---
+++ Assets/Scripts/ScObject.cs
@@ -0,0 +1,49 @@
+﻿using BzKovSoft.ObjectSlicer;
+using BzKovSoft.ObjectSlicer.EventHandlers;
+using System;
+using UnityEngine;
+
+
+namespace SliceCase
+{
+    public class ScObject : BzSliceableObjectBase, IBzObjectSlicedEvent
+    {
+        private int sliceId = -1;
+
+
+        public void Slice(Plane plane, int sliceId, Action<BzSliceTryResult> callBack)
+        {
+            if (this.sliceId == sliceId)
+            {
+                callBack?.Invoke(new BzSliceTryResult(false, null));
+                return;
+            }
+
+            this.sliceId = sliceId;
+            Slice(plane, callBack);
+        }
+
+
+        protected override BzSliceTryData PrepareData(Plane plane)
+        {
+            var colliders = gameObject.GetComponentsInChildren<Collider>();
+            return new BzSliceTryData
+            {
+                addData = null, componentManager = new StaticComponentManager(gameObject, plane, colliders),
+                plane = plane
+            };
+        }
+
+
+        protected override void OnSliceFinished(BzSliceTryResult result) { }
+
+
+        public void ObjectSliced(GameObject original, GameObject resultNeg, GameObject resultPos)
+        {
+            if (resultPos != null)
+            {
+                Destroy(resultPos);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ScPlane.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace SliceCase
+{
+    public class ScPlane : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField] private Vector3 boxCastSize;
+        [SerializeField] private Transform cutPointRoot;
+
+
+        private RaycastHit[] hitBuffer;
+        private HashSet<ScObject> sliceables;
+
+        #endregion
+
+
+
+        #region Unity lifecycle
+
+        private void Awake()
+        {
+            hitBuffer = new RaycastHit[20];
+            sliceables = new HashSet<ScObject>();
+        }
+
+
+        private void Update()
+        {
+            int hitsCount = Physics.BoxCastNonAlloc(cutPointRoot.position + boxCastSize.y * cutPointRoot.up,
+                0.5f * boxCastSize, -cutPointRoot.up, hitBuffer);
+
+            if (hitsCount == 0)
+            {
+                return;
+            }
+
+            sliceables.Clear();
+
+            for (int i = 0; i < hitsCount; i++)
+            {
+                ScObject s = hitBuffer[i].collider.gameObject.GetComponentInParent<ScObject>();
+                if (s != null)
+                {
+                    sliceables.Add(s);
+                }
+            }
+
+            Plane cutPlane = new Plane(transform.forward, transform.position);
+            foreach (var s in sliceables)
+            {
+                s.Slice(cutPlane, Time.frameCount, result => { });
+            }
+        }
+
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(transform.position, boxCastSize);
+        }
+
+        #endregion
+    }
+}
